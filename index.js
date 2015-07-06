0515489352b317659a2ce8a7bc4c72751bd92d77
vijaykani <e.vijaykani@gmail.com>
2015-07-06 22:00:14 +0530

    First Commit

---
+++ index.js
@@ -0,0 +1,15 @@
+﻿var express = require('express');
+var app = express();
+var http = require('http');
+var path = require('path'); 
+
+app.set("view engine", "html");
+app.engine('html', require('ejs').renderFile);
+app.set('views', path.join(__dirname , 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/To-Do', function (req, res) {
+    res.render("index");
+});
+
+http.createServer(app).listen(5000);
